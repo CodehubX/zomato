ec1862608c4da831a1f04ad80804896df1ad1e8c
Rajneesh Shukla <rajneesh9651@gmail.com>
2018-11-02 16:27:34 +0530

    Add dummy Navigation drawer provided by Android Studio

--- app/src/main/java/com/example/rajneeshshukla/zometo/activities/splash/SplashActivity.java
+++ app/src/main/java/com/example/rajneeshshukla/zometo/activities/splash/SplashActivity.java
@@ -1,17 +1,20 @@
-package com.example.rajneeshshukla.zometo;
+package com.example.rajneeshshukla.zometo.activities.splash;
 
 import android.content.Intent;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Menu;
+
+import com.example.rajneeshshukla.zometo.HomeActivity;
+import com.example.rajneeshshukla.zometo.R;
+import com.example.rajneeshshukla.zometo.activities.home.MainActivity;
 
 /**
  *  Class  is used to display Splash on start of the app
  */
 public class SplashActivity extends AppCompatActivity {
 
-    final private int SPLASH_SCREEN_TIMEOUT = 1000;
+    final private int SPLASH_SCREEN_TIMEOUT = 2000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,7 +30,7 @@ public class SplashActivity extends AppCompatActivity {
         handler.postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent i = new Intent(getApplicationContext(), MainActivity.class);
+                Intent i = new Intent(getApplicationContext(), HomeActivity.class);
                 startActivity(i);
             }
         }, SPLASH_SCREEN_TIMEOUT);
---
+++ app/src/main/java/com/example/rajneeshshukla/zometo/utilities/Utility.java
@@ -0,0 +1,4 @@
+package com.example.rajneeshshukla.zometo.utilities;
+
+public class Utility {
+}
